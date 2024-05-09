a04e57920099547199768ca701d6b54741cacc35
cailenfisher <cailen.fisher@gmail.com>
2024-05-08 20:43:43 -0400

    tweaks

--- src/lib/components/Survey/Survey.svelte
+++ src/lib/components/Survey/Survey.svelte
@@ -93,7 +93,7 @@
                 <label
               for="issue"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-              >Description of issue</label
+              >Medical issue</label
             >
             <textarea
               id="issue"
@@ -105,15 +105,15 @@
             </div>
             <div><label
                 for="experience"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                >Description of your experience with the ER, including any concerns</label
+                class="block mt-6 mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >Your experience with the ER, including any concerns</label
               >
               <textarea
                 id="experience"
                 name="experience"
                 rows="6"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Summary of what brought you to the ER"
+                placeholder="Summary of what happened during your treatment"
               ></textarea></div>
             <div class="mt-4">
               <div>
